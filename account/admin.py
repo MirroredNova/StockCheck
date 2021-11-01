8b79e9ae2119c598b0de2911e6e4659ad917d02f
MirroredNova <anethercrafter@gmail.com>
2021-11-01 02:17:31 -0500

    added discord and phone fields

--- account/admin.py
+++ account/admin.py
@@ -3,12 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 
 from account.models import User
 
+
 # Register your models here.
+class User_admin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email', 'phone', 'discord']
 
 
-class CustomUserAdmin(UserAdmin):
-    model = User
-    list_display = ['username', 'email', 'first_name', 'last_name']
-
-
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User, User_admin)
--- account/forms.py
+++ account/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
+from phonenumber_field.formfields import PhoneNumberField
 
 
 class CreateUserForm(UserCreationForm):
 
     class Meta(UserCreationForm):
+        phone = PhoneNumberField()
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email', 'phone', 'discord')
 
 
 class LogInForm(forms.Form):

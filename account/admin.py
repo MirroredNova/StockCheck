d8cac0e1008236ca04897b0b248a4f7f66685a10
MirroredNova <anethercrafter@gmail.com>
2021-11-04 22:00:52 -0500

    account management functioning

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from account.models import User
 
 
 # Register your models here.
-class User_admin(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'first_name', 'last_name', 'email', 'phone', 'discord']
 
 
-admin.site.register(User, User_admin)
+admin.site.register(User, UserAdmin)
--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 from phonenumber_field.formfields import PhoneNumberField
 
@@ -15,3 +15,20 @@ class CreateUserForm(UserCreationForm):
 class LogInForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class AccountManagementForm(forms.ModelForm):
+    # def __init__(self, user, *args, **kwargs):
+    #     self.user = user
+    #     super(AccountManagementForm, self).__init__(*args, **kwargs)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+    phone = PhoneNumberField()
+    discord = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone', 'discord']
+
+

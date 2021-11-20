2a9324f03f1665d81b28d9139d90e5f25c330cb7
Jeremy <japeplin@gmail.com>
2021-11-19 23:44:08 -0600

    Further scraper_service test implementation
    Further tests added, though I haven't been able to get mocks to work properly for testing of update_products.  On the upside, this did improve coverage of scrapers slightly.

--- products/tests/test_scraper_service.py
+++ products/tests/test_scraper_service.py
@@ -1,4 +1,10 @@
 from django.test import TestCase
+import unittest
+from unittest.mock import patch
+from unittest.mock import MagicMock
+
+import products.scrapers.best_buy_scraper
+from products.scrapers.best_buy_scraper import BestBuyScraper
 from products.scrapers.scraper_service import RunScraper
 from products.scrapers.scraper_service import NotificationSender
 from products.scrapers.scraper_service import main
@@ -31,18 +37,6 @@ class ScraperServiceTestCases(TestCase):
         # object = Product.objects.get(product_name='1')
         # self.assertEqual(RunScraper.product_dict['1'], object.last_updated, "Unexpected value for element in dict")
 
-    def test_create_dict_one(self):
-        """Only a single product in the database should result in a matching dict"""
-        Product.objects.create(supplier='Amazon', current_stock=False, current_price=10.00, product_id='15',
-                               last_updated=datetime.now().time(), product_name='1', product_nickname='One',
-                               product_url='nya.com')
-        # With no return value, I'll start by just running this function and confirming that no errors appear
-        RunScraper.create_product_dict(self.run_scraper)
-
-        # Once the product_dict is handled, this should help confirm things
-        # object = Product.objects.get(product_name='1')
-        # self.assertEqual(RunScraper.product_dict['1'], object.last_updated, "Unexpected value for element in dict")
-
     def test_create_dict_several(self):
         """Several products in the database should result in a matching dict"""
         Product.objects.create(supplier='Amazon', current_stock=False, current_price=10.00, product_id='15',
@@ -59,5 +53,75 @@ class ScraperServiceTestCases(TestCase):
         RunScraper.create_product_dict(self.run_scraper)
 
         # Once the product_dict is handled, this should help confirm things
-        # object = Product.objects.get(product_name='3')
-        # self.assertEqual(RunScraper.product_dict['3'], object.last_updated, "Unexpected value for element in dict")
+        # an_object = Product.objects.get(product_name='3')
+        # self.assertEqual(RunScraper.product_dict['3'], an_object.last_updated, "Unexpected value for element in dict")
+
+    @patch('products.scrapers.best_buy_scraper.BestBuyScraper')
+    @patch('products.scrapers.amazon_scraper.amazon_scraper')
+    def test_update_products_empty(self, mock_best_buy_scraper, mock_amazon_scraper):
+        """Updating an empty list of products should not cause any uncaught errors"""
+
+        # Setting up mocks
+        mock_best_buy_scraper.get_price_bestbuy.return_value =\
+            (False, 42.00, "2")
+        mock_best_buy_scraper.get_product_url_bestbuy.return_value = 'nya2.net'
+        mock_amazon_scraper.return_value = [(True, 9.50, "1"), (True, 0.05, "3")]
+
+        # Running product update
+        RunScraper.update_products(self.run_scraper, Product.objects.all())
+
+    @patch('products.scrapers.best_buy_scraper.BestBuyScraper')
+    @patch('products.scrapers.amazon_scraper.amazon_scraper')
+    def test_update_products_one_best_buy(self, mock_best_buy_scraper, mock_amazon_scraper):
+        """Updating a list with a single product from Best Buy should change stock from true to false"""
+
+        # Setting up mocks
+        mock_best_buy_scraper.get_price_bestbuy.return_value = \
+            (False, 42.00, "2")
+        mock_best_buy_scraper.get_product_url_bestbuy.return_value = 'nya2.net'
+        mock_amazon_scraper.return_value = [(True, 9.50, "1"), (True, 0.05, "3")]
+
+        # Populating database
+        Product.objects.create(supplier='bestbuy', current_stock=True, current_price=42.00, product_id='B012',
+                               last_updated=datetime.now().time(), product_name='2', product_nickname='Two',
+                               product_url='nya2.net')
+
+        # Storing old version of product object's timestamp data
+        original_timestamp = Product.objects.get(product_name='2').last_updated
+
+        # Running product update
+        RunScraper.update_products(self.run_scraper, Product.objects.all())
+
+        # Grabbing full object and testing for changes
+        best_buy_object = Product.objects.get(product_name='2')
+        self.assertFalse(best_buy_object.current_stock, "Update to product should have changed stock to out-of-stock")
+        self.assertNotEqual(best_buy_object.last_updated, original_timestamp,
+                            "Updating product should change the timestamp")
+
+    @patch('products.scrapers.best_buy_scraper.BestBuyScraper')
+    @patch('products.scrapers.amazon_scraper.amazon_scraper')
+    def test_update_products_one_amazon(self, mock_best_buy_scraper, mock_amazon_scraper):
+        """Updating a list with a single product from Amazon should change stock from true to false"""
+
+        # Setting up mocks
+        mock_best_buy_scraper.get_price_bestbuy.return_value = (False, 42.00, "2")
+        mock_best_buy_scraper.get_product_url_bestbuy.return_value = 'nya2.net'
+        mock_amazon_scraper.return_value = (False, 0.05, "3")
+
+        # Populating database
+        Product.objects.create(supplier='Amazon', current_stock=True, current_price=0.01,
+                               product_id='ABCDEFGHIJKLMNOPQRSTUVWXYZ1234', last_updated=datetime.now().time(),
+                               product_name='3', product_nickname='Three', product_url='nya3.org')
+
+        # Storing old version of product object's timestamp data
+        original_timestamp = Product.objects.get(product_name='3').last_updated
+
+        # Running product update
+        RunScraper.update_products(self.run_scraper, Product.objects.all())
+
+        # Grabbing full object and testing for changes
+        best_buy_object = Product.objects.get(product_name='3')
+        self.assertFalse(best_buy_object.current_stock, "Update to product should have changed stock to out-of-stock")
+        self.assertNotEqual(best_buy_object.last_updated, original_timestamp,
+                            "Updating product should change the timestamp")
+        
